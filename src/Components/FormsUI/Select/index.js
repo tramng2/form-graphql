cacc59d4efcb691b7ca1d5f57e8eb718b77c7f27
TramNguyen <tramtram2205@gmail.com>
2021-05-19 03:11:43 +0300

    Select states after choosing a country

--- src/Components/FormsUI/Select/index.js
+++ src/Components/FormsUI/Select/index.js
@@ -1,81 +1,96 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React, { useEffect } from "react";
 import InputLabel from "@material-ui/core/InputLabel";
 import Grid from "@material-ui/core/Grid";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
-import { CountryRegionData } from "react-country-region-selector";
 
-const SelectWrapper = ({
-    name,
-    options,
-    ...otherProps
-}) => {
-
-    // const configSelect = {
-    //     select: true,
-    //     variant: 'outlined',
-    //     fullWidth: true,
-    // };
-
+const SelectWrapper = () => {
+    const [countries, setCountries] = React.useState([]);
     const [country, setCountry] = React.useState("");
-    const [regions, setRegions] = React.useState([]);
-    const [city, setCity] = React.useState("");
+    const [states, setStates] = React.useState([]);
+    const [state, setState] = React.useState([]);
+
+
+    useEffect(() => {
+        fetch("https://countries.trevorblades.com/", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                query: `
+                query {
+                    countries {
+                     name
+                     code
+                    }
+                   }
+                `
+            })
+        })
+            .then(res => res.json())
+            .then(data => { setCountries(data.data.countries) })
+    }, [])
+
+    useEffect(() => {
+        fetch("https://countries.trevorblades.com/", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                query: `
+                query getCities ($code: ID!){
+                    country (code: $code) {
+                    states {
+                      name
+                    }
+                  }
+                  }
+                `,
+                variables: { code: country }
+            })
+        })
+            .then(res => res.json())
+            .then(data => { setStates(data.data.country.states) })
+            .catch(er => console.log("Error", er))
+    }, [country])
+
+
 
     const handleChangeCountry = (event) => {
         setCountry(event.target.value);
-        setRegions(getRegions(event.target.value));
     };
-    const handleChangeCity = (event) => {
-        console.log(event.target.value)
-        setCity(event.target.value);
+    const handleChangeState = (event) => {
+        setState(event.target.value);
     };
-
-    const getRegions = (country) => {
-        if (!country) {
-            console.log("No country yet");
-            return []
-        } else {
-            let [cityArr] = CountryRegionData.filter((el) => el[0] === country);
-            if (cityArr) {
-                return cityArr[2].split("|").map((regionPair) => {
-                    let [regionName, regionShortCode = null] = regionPair.split("~");
-                    return regionName;
-                });
-            }
-        }
-    };
-
     return (
         <Grid container spacing={2}>
             <Grid item xs={6}>
-                <FormControl variant="outlined" fullWidth= "true">
+                <FormControl variant="outlined" fullWidth="true">
                     <InputLabel>Country</InputLabel>
-                    <Select value={country} onChange={handleChangeCountry} label="country">
-                        {CountryRegionData.map((countryArr, index) => (
-                            <MenuItem value={countryArr[0]} key={index}>
-                                {countryArr[0]}
+                    <Select value={country} onChange={handleChangeCountry} label="country" displayEmpty="false">
+                        {countries.map((countryObj, index) => (
+                            <MenuItem value={countryObj.code} key={index}>
+                                {countryObj.name}
                             </MenuItem>
                         ))}
                     </Select>
                 </FormControl>
             </Grid>
             <Grid item xs={6}>
-                {country ? (
-                    <FormControl variant="outlined" fullWidth= "true">
-                        <InputLabel>City</InputLabel>
-                        <Select value={city} onChange={handleChangeCity} label="city">
-                            {regions.map((city, index) => (
-                                <MenuItem key={index} value={city}>{city}</MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                ) : null}
+                <FormControl variant="outlined" fullWidth="true">
+                    <InputLabel>State</InputLabel>
+                    <Select value={state} onChange={handleChangeState} label="state" >
+                        {country ? (states.map((state, index) => (
+                            <MenuItem key={index} value={state.name}>{state.name}</MenuItem>
+                        ))) : <MenuItem>Please choose your country</MenuItem> }
+                    </Select>
+                </FormControl>
             </Grid>
         </Grid>
-
     );
-};
+}
 
 export default SelectWrapper;
