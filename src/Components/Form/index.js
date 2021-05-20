38dbd7fa725fe40109f6cf1805a4b928924a4b62
TramNguyen <tramtram2205@gmail.com>
2021-05-21 00:10:54 +0300

    Apply submit button.

---
+++ src/Components/Form/index.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import Header from '../Header';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Container,
+  Grid,
+  Typography
+} from '@material-ui/core';
+import Textfield from '../FormsUI/Textfield';
+import Select from '../FormsUI/Select';
+import Button from '../FormsUI/Button';
+
+const useStyles = makeStyles((theme) => ({
+  formWrapper: {
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(8),
+  },
+}));
+
+const INITIAL_FORM_STATE = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  phone: '',
+  addressLine: '',
+};
+
+const FORM_VALIDATION = Yup.object().shape({
+  firstName: Yup.string()
+    .required('Required'),
+  lastName: Yup.string()
+    .required('Required'),
+  email: Yup.string()
+    .email('Invalid email.')
+    .required('Required'),
+  phone: Yup.number()
+    .integer()
+    .typeError('Please enter a valid phone number')
+    .required('Required'),
+  addressLine: Yup.string()
+    .required('Required'),
+
+});
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <Header />
+      </Grid>
+      <Grid item xs={12}>
+        <Container maxWidth="md">
+          <div className={classes.formWrapper}>
+
+            <Formik
+              initialValues={{
+                ...INITIAL_FORM_STATE
+              }}
+              validationSchema={FORM_VALIDATION}
+              onSubmit={values => {
+                console.log(values);
+              }}
+            >
+              <Form>
+
+                <Grid container spacing={2}>
+
+                  <Grid item xs={12}>
+                    <Typography>
+                      Your details
+                    </Typography>
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <Textfield
+                      name="firstName"
+                      label="First Name"
+                    />
+                  </Grid>
+
+                  <Grid item xs={6}>
+                    <Textfield
+                      name="lastName"
+                      label="Last Name"
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Textfield
+                      name="email"
+                      label="Email"
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Textfield
+                      name="phone"
+                      label="Phone"
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Typography>
+                      Address
+                    </Typography>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Textfield
+                      name="addressLine"
+                      label="Address Line "
+                    />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Select
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button>
+                      Submit Form
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Form>
+            </Formik>
+
+          </div>
+        </Container>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default App;
--- src/Components/FormsUI/Select/index.js
+++ src/Components/FormsUI/Select/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import InputLabel from "@material-ui/core/InputLabel";
 import Grid from "@material-ui/core/Grid";
 import MenuItem from "@material-ui/core/MenuItem";
@@ -6,11 +6,10 @@ import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 
 const SelectWrapper = () => {
-    const [countries, setCountries] = React.useState([]);
-    const [country, setCountry] = React.useState("");
-    const [states, setStates] = React.useState([]);
-    const [state, setState] = React.useState([]);
-
+    const [countries, setCountries] = useState([]);
+    const [country, setCountry] = useState("");
+    const [states, setStates] = useState([]);
+    const [state, setState] = useState([]);
 
     useEffect(() => {
         fetch("https://countries.trevorblades.com/", {
@@ -26,7 +25,7 @@ const SelectWrapper = () => {
                      code
                     }
                    }
-                `
+ `
             })
         })
             .then(res => res.json())
@@ -48,29 +47,32 @@ const SelectWrapper = () => {
                     }
                   }
                   }
-                `,
+ `,
                 variables: { code: country }
             })
         })
             .then(res => res.json())
-            .then(data => { setStates(data.data.country.states) })
+            .then(data => {
+                setStates(data.data.country.states)
+            })
             .catch(er => console.log("Error", er))
-    }, [country])
-
-
+    }, [country, states.length])
 
     const handleChangeCountry = (event) => {
         setCountry(event.target.value);
+
     };
     const handleChangeState = (event) => {
         setState(event.target.value);
     };
+
+
     return (
         <Grid container spacing={2}>
             <Grid item xs={6}>
                 <FormControl variant="outlined" fullWidth="true">
-                    <InputLabel>Country</InputLabel>
-                    <Select value={country} onChange={handleChangeCountry} label="country" displayEmpty="false">
+                    <InputLabel>Choose country</InputLabel>
+                    <Select value={country} onChange={handleChangeCountry} label="Choose country">
                         {countries.map((countryObj, index) => (
                             <MenuItem value={countryObj.code} key={index}>
                                 {countryObj.name}
@@ -80,17 +82,33 @@ const SelectWrapper = () => {
                 </FormControl>
             </Grid>
             <Grid item xs={6}>
-                <FormControl variant="outlined" fullWidth="true">
-                    <InputLabel>State</InputLabel>
-                    <Select value={state} onChange={handleChangeState} label="state" >
-                        {country ? (states.map((state, index) => (
-                            <MenuItem key={index} value={state.name}>{state.name}</MenuItem>
-                        ))) : <MenuItem>Please choose your country</MenuItem> }
-                    </Select>
+                <FormControl variant="outlined" fullWidth="true" >
+                    {country && states ? (
+                        states.length !== 0 ? (
+                            <>
+                                <InputLabel>Choose state</InputLabel>
+                                <Select value={state} onChange={handleChangeState} label="Choose state">
+                                    {states.map((state, index) => (
+                                        <MenuItem value={state.name} key={index}>
+                                            {state.name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </>
+                        ) : <>
+                            <InputLabel>Sorry, states are not available.</InputLabel>
+                            <Select>
+                            </Select>
+                        </>
+                    ) : (<>
+                        <InputLabel>Sorry, please choose country first.</InputLabel>
+                        <Select>
+                        </Select>
+                    </>
+                    )}
                 </FormControl>
             </Grid>
         </Grid>
     );
 }
-
 export default SelectWrapper;
